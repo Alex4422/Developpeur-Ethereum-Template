b731070852e5c1f9861b4698d681e9055c046b5f
Alex <alexandregautier82@gmail.com>
2021-11-13 11:59:52 +0100

    10-TP_Crowdsale OK

---
+++ 3_Solidity/contracts/10-TP_Crowdsale/ERC20Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20{
+
+    constructor(uint256 initialSupply) public ERC20("ALYRA","ALY") {
+        _mint(msg.sender, initialSupply);
+    }
+
+}
