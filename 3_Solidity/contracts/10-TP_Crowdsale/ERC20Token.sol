186fc08439a32cef4bfeb1c8564d261316711d95
Alex <alexandregautier82@gmail.com>
2021-11-14 11:31:18 +0100

    modification of the pragma & adding of package.json

--- 3_Solidity/contracts/10-TP_Crowdsale/ERC20Token.sol
+++ 3_Solidity/contracts/10-TP_Crowdsale/ERC20Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.7;
+pragma solidity 0.8.9;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
