2aaa851f0ff8afc91a94fede35891d4284d0bb37
Alex <alexandregautier82@gmail.com>
2021-11-16 23:28:36 +0100

    3-Exer_TDD_ERC20 OK

---
+++ 4_Truffle_et_CI-CD/3-Exer_TDD_ERC20/test/ERC20.test.js
@@ -0,0 +1,124 @@
+// erc20.test.js
+const { BN, ether, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const ERC20 = artifacts.require('ERC20Token');
+
+contract('ERC20', function (accounts) {
+
+
+        const _name = 'ALYRA';
+        const _symbol = 'ALY';
+        const _initialsupply = new BN(1000);
+        const _decimals = new BN(18);
+        const owner = accounts[0];
+        const recipient = accounts[1];
+        const recipient2 = accounts[2];
+
+
+        beforeEach(async function () {
+            this.ERC20Instance = await ERC20.new(_initialsupply, {from: owner});
+        });
+
+
+        it('a un nom', async function () {
+
+            expect(await this.ERC20Instance.name()).to.equal(_name);
+        });
+
+        it('a un symbole', async function () {
+
+            expect(await this.ERC20Instance.symbol()).to.equal(_symbol);
+        });
+
+        it('a 1 valeur décimale', async function() {
+
+            expect(await this.ERC20Instance.decimals()).to.be.bignumber.equal(_decimals);
+        });
+
+        it('vérifie la balance du propriétaire du contrat', async function() {
+
+            let balanceOwner = await this.ERC20Instance.balanceOf(owner);
+            let totalSupply = await this.ERC20Instance.totalSupply();
+
+            expect(balanceOwner).to.be.bignumber.equal(totalSupply);
+        });
+
+        it("vérifie si un transfert a bien été effectué", async function() {
+
+            let balanceOwnerBeforeTransfer = await this.ERC20Instance.balanceOf(owner);
+            let balanceRecipientBeforeTransfer = await this.ERC20Instance.balanceOf(recipient);
+
+            let amount = new BN(10);
+            await this.ERC20Instance.transfer(recipient, amount, {from: owner});
+
+            let balanceOwnerAfterTransfer = await this.ERC20Instance.balanceOf(owner);
+            let balanceRecipientAfterTransfer = await this.ERC20Instance.balanceOf(recipient);
+
+            expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
+            expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
+        });
+
+        it("vérifie si la fonction approve marche", async function() {
+
+            //function approve(address _spender, uint256 _value) public returns (bool success)
+            //function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+
+            let amount = new BN(10);
+            await this.ERC20Instance.approve(recipient, amount, {from:owner});
+
+            let allowanceAfter1 = await this.ERC20Instance.allowance(owner, recipient);
+            expect(allowanceAfter1).to.be.bignumber.equal(amount);
+
+            let amount2 = new BN(100);
+            await this.ERC20Instance.approve(recipient, amount2, {from:owner});
+
+            let allowanceAfter2 = await this.ERC20Instance.allowance(owner, recipient);
+            expect(allowanceAfter2).to.be.bignumber.equal(amount2);
+        });
+
+        it("Vérifie si transferFrom marche", async function(){
+
+            //function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
+
+            let amount1 = new BN(10);
+
+            let ownerBalanceBeforeTransfer = await this.ERC20Instance.balanceOf(owner);
+            let recipient2BalanceBeforeTransfer = await this.ERC20Instance.balanceOf(recipient2);
+
+            await this.ERC20Instance.approve(recipient, amount1, {from:owner});
+            await this.ERC20Instance.transferFrom(owner, recipient2, amount1, {from: recipient});
+
+            let ownerBalanceAfterTransfer = await this.ERC20Instance.balanceOf(owner);
+            let recipient2BalanceAfterTransfer = await this.ERC20Instance.balanceOf(recipient2);
+
+            expect(ownerBalanceAfterTransfer).to.be.bignumber.equal(ownerBalanceBeforeTransfer.sub(amount1));
+            expect(recipient2BalanceAfterTransfer).to.be.bignumber.equal(recipient2BalanceBeforeTransfer.add(amount1));
+        });
+
+        it("Checks the revert the transferFrom from", async function() {
+
+            let amount = new BN(10);
+            await expectRevert(this.ERC20Instance.transferFrom(owner, recipient, amount, {from: recipient}),
+                "ERC20: transfer amount exceeds allowance");
+        });
+
+        it("vérifie la mise à jour de la fonction approve après le transferFrom", async function() {
+
+            let amount = new BN(10);
+
+            await this.ERC20Instance.approve(recipient, new BN(100), {from:owner});
+            await this.ERC20Instance.transferFrom(owner, recipient2, amount, {from: recipient});
+
+            let allowanceAfter = await this.ERC20Instance.allowance(owner, recipient);
+
+            expect(allowanceAfter).to.be.bignumber.equal(new BN(90));
+
+        });
+});
+
+
+
+
+
+
+
